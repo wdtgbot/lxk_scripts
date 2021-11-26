5e7fc7b44d7eb18e0c5e604fbfe5b3a6045a0bc6
jiulan <37990936+jiulan@users.noreply.github.com>
2021-11-26 14:55:50 +0800

    fix

--- jd_redPacket.js
+++ jd_redPacket.js
@@ -66,42 +66,42 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
       // await showMsg();
     }
   }
-  for (let i = 0; i < cookiesArr.length; i++) {
-    cookie = cookiesArr[i];
-    $.index = i + 1;
-    $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-    $.canHelp = true;
-    $.redPacketId = [...new Set($.redPacketId)];
-    if (!isLoginInfo[$.UserName]) continue
-    if (cookiesArr && cookiesArr.length >= 2) {
-      console.log(`\n\n自己账号内部互助`);
-      for (let j = 0; j < $.redPacketId.length && $.canHelp; j++) {
-        console.log(`账号 ${$.index} ${$.UserName} 开始给 ${$.redPacketId[j]} 进行助力`)
-        $.max = false;
-        await jinli_h5assist($.redPacketId[j]);
-        await $.wait(2000)
-        if ($.max) {
-          $.redPacketId.splice(j, 1)
-          j--
-          continue
-        }
-      }
-    }
-    // if ($.canHelp && ($.authorMyShareIds && $.authorMyShareIds.length)) {
-    //   console.log(`\n\n有剩余助力机会则给作者进行助力`);
-    //   for (let j = 0; j < $.authorMyShareIds.length && $.canHelp; j++) {
-    //     console.log(`\n账号 ${$.index} ${$.UserName} 开始给作者 ${$.authorMyShareIds[j]} 进行助力`)
-    //     $.max = false;
-    //     await jinli_h5assist($.authorMyShareIds[j]);
-    //     await $.wait(2000)
-    //     if ($.max) {
-    //       $.authorMyShareIds.splice(j, 1)
-    //       j--
-    //       continue
-    //     }
-    //   }
-    // }
-  }
+  // for (let i = 0; i < cookiesArr.length; i++) {
+  //   cookie = cookiesArr[i];
+  //   $.index = i + 1;
+  //   $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
+  //   $.canHelp = true;
+  //   $.redPacketId = [...new Set($.redPacketId)];
+  //   if (!isLoginInfo[$.UserName]) continue
+  //   if (cookiesArr && cookiesArr.length >= 2) {
+  //     console.log(`\n\n自己账号内部互助`);
+  //     for (let j = 0; j < $.redPacketId.length && $.canHelp; j++) {
+  //       console.log(`账号 ${$.index} ${$.UserName} 开始给 ${$.redPacketId[j]} 进行助力`)
+  //       $.max = false;
+  //       await jinli_h5assist($.redPacketId[j]);
+  //       await $.wait(2000)
+  //       if ($.max) {
+  //         $.redPacketId.splice(j, 1)
+  //         j--
+  //         continue
+  //       }
+  //     }
+  //   }
+  //   // if ($.canHelp && ($.authorMyShareIds && $.authorMyShareIds.length)) {
+  //   //   console.log(`\n\n有剩余助力机会则给作者进行助力`);
+  //   //   for (let j = 0; j < $.authorMyShareIds.length && $.canHelp; j++) {
+  //   //     console.log(`\n账号 ${$.index} ${$.UserName} 开始给作者 ${$.authorMyShareIds[j]} 进行助力`)
+  //   //     $.max = false;
+  //   //     await jinli_h5assist($.authorMyShareIds[j]);
+  //   //     await $.wait(2000)
+  //   //     if ($.max) {
+  //   //       $.authorMyShareIds.splice(j, 1)
+  //   //       j--
+  //   //       continue
+  //   //     }
+  //   //   }
+  //   // }
+  // }
 })()
     .catch((e) => {
       $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
